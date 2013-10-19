245c1b57487efdddc0bc4308171c385b79c639d3
cicimen <egemengulkilik@gmail.com>
2013-10-19 12:07:10 +0300

    19.10.2013-commiti

---
+++ SportsStore.WebUI/Infrastructure/Concrete/FormsAuthProvider..cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Security;
+using SportsStore.WebUI.Infrastructure.Abstract;
+
+
+namespace SportsStore.WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string username, string password)
+        {
+            //önerilen membership API
+
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+            }
+            return result;
+        }
+    }
+}
